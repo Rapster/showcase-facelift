83adf60c740888fda92300bb6f79507d1906a1df
Rapster <sebastien.lepage@outlook.com>
2018-06-10 19:20:32 +0200

    Fix #1 - Don't insert code using <pre />

--- src/main/java/org/primefaces/showcase/util/ShowcaseUtil.java
+++ src/main/java/org/primefaces/showcase/util/ShowcaseUtil.java
@@ -18,34 +18,58 @@
 
 package org.primefaces.showcase.util;
 
-import javax.faces.context.FacesContext;
 import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+import javax.faces.context.FacesContext;
+
+import org.omnifaces.util.Faces;
 
 /**
  * ShowcaseUtil
  *
- * @author  Pavol Slany / last modified by $Author$
+ * @author Pavol Slany / last modified by $Author$
  * @version $Revision$
  */
 public class ShowcaseUtil {
-	public static String getFileContent(final String fullPathToFile) {
-		try {
-			// Finding in WEB ...
-			FacesContext fc = FacesContext.getCurrentInstance();
-			InputStream is = fc.getExternalContext().getResourceAsStream(fullPathToFile);
-			if (is != null) {
-				return FileContentMarkerUtil.readFileContent(fullPathToFile, is);
-			}
 
-			// Finding in ClassPath ...
-			is = ShowcaseUtil.class.getResourceAsStream(fullPathToFile);
-			if (is != null) {
-                return FileContentMarkerUtil.readFileContent(fullPathToFile, is);
-			}
-		} catch (Exception e) {
-			throw new IllegalStateException("Internal error: file " + fullPathToFile + " could not be read", e);
-		}
+    public static FileContent getFileContent(final String fullPathToFile, Boolean readBeans) {
+        try {
+            // Finding in WEB ...
+            FacesContext fc = FacesContext.getCurrentInstance();
+            InputStream is = fc.getExternalContext().getResourceAsStream(fullPathToFile);
+            if (is != null) {
+                return FileContentMarkerUtil.readFileContent(fullPathToFile, is, readBeans);
+            }
 
-		return "";
-	}
+            // Finding in ClassPath ...
+            is = ShowcaseUtil.class.getResourceAsStream(fullPathToFile);
+            if (is != null) {
+                return FileContentMarkerUtil.readFileContent(fullPathToFile, is, readBeans);
+            }
+        } catch (Exception e) {
+            throw new IllegalStateException("Internal error: file " + fullPathToFile + " could not be read", e);
+        }
+
+        return null;
+    }
+
+    public static final List<FileContent> getCurrentFlatFilesContent() {
+        return getFlatFilesContent(Faces.getRequestServletPath());
+    }
+
+    public static final List<FileContent> getFlatFilesContent(String fullPathToFile) {
+        FileContent content = getFileContent(fullPathToFile, true);
+        List<FileContent> flatFiles = new ArrayList<>();
+
+        if (content != null) {
+            flatFiles.add(new FileContent(content.getTitle(), content.getValue(), content.getType(), Collections.<FileContent>emptyList()));
+            for (FileContent c : content.getAttached()) {
+                flatFiles.add(c);
+            }
+        }
+        return flatFiles;
+    }
 }
